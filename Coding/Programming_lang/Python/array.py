e8b4e1f3f1a8449f2a968c8c1ab744bae075614c
S M Mahsanul Islam Nirjhor <mahsanulnirjhor@gmail.com>
2023-09-20 16:53:29 +0600

    array.py file added

---
+++ Coding/Programming_lang/Python/array.py
@@ -0,0 +1,14 @@
+a = [1,2,3,4,5]
+print(a)
+
+print(len(a))
+
+print(a[0:])
+
+print(a[-1]) # => Top of stack
+
+a.append(6)
+a.append(7)
+print(a)
+
+print(a.pop()) # => Pop out the top element from the stack
